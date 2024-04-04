8450fb5da4a16e7ba3f24f056523619a8e2081b3
deborah <fabbydebby@gmail.com>
2024-04-04 14:37:01 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+// function submitData(name, email) {
+//     const url = 'http://localhost:3000/users'
+
+//     const data = {
+//         name: name,
+//         email: email
+//     };
+// }
+function submitData(name, email) {
+  const url = "http://localhost:3000/users";
+
+  const data = {
+    name: name,
+    email: email
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(data)
+  };
+
+  return fetch(url, options)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("network response was not ok");
+        }
+        return response.json();
+    })
+    .then((data) => {
+      const userId = data.id;
+      document.body.textContent = `New user ID: ${userId}`;
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+      document.body.textContent = `Error: ${error.message}`;
+
+      return error;
+    });
+}
+
+
